109fb0a2c5a0a8dcc295a5e5f4af03d5bf46a186
NikitaVolia <volanskijnikita2@gmail.com>
2025-11-19 23:32:24 +0100

    Add main page documantation with Doxyfile config file

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/**
+ * @mainpage MLV Snake
+ *
+ * Welcome to the documentation for the **MLV Snake** project.
+ *
+ * ## Project Overview
+ * **MLV Snake** is a team project developed for the course *Imperative Programming 2* 
+ * (L2 Informatique, 2nd year). The project is a classic implementation of the Snake game 
+ * using the **MLV library** for graphics and input handling.
+ *
+ * ## Project Team
+ * - **VOLIANSKYI Nikita**
+ * - **MELKONIAN Mark**
+ *
+ * @see Snake
+ * @see GameConfig
+ */
+
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
